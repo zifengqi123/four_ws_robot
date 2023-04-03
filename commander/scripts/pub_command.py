dac993fd1dcaa551b02cbce5fcf7713afa7498cd
5met4nka <pavilich99@mail.ru>
2023-04-03 12:56:39 +0300

    нода управления переписана под crab walk

--- commander/scripts/pub_command.py
+++ commander/scripts/pub_command.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import rospy
 
-from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
@@ -10,23 +9,17 @@ vel_msg = Twist()
 def callback_controller(data):
     global vel_msg
 
-    # управление только левым стиком
-    
     vel_msg.linear.x = data.axes[1]
+    vel_msg.linear.y = data.axes[3]
     vel_msg.angular.z = data.axes[0]
 
 def commander():
     global vel_msg
 
-    head_angular_speed = 8
-
     pub = rospy.Publisher('/four_wheel_steering_controller/cmd_vel', Twist, queue_size = 10)
-    pub_head = rospy.Publisher('/lidar_head_controller/command', Float64, queue_size = 10)
     rate = rospy.Rate(50)
-
     while not rospy.is_shutdown():
         pub.publish(vel_msg)
-        pub_head.publish(head_angular_speed)
         rate.sleep()
 
 if __name__ == '__main__':
