5c699f3c63049b35234a5920a8170c1914b3bb3b
5met4nka <pavilich99@mail.ru>
2023-05-10 21:10:17 +0300

    устанавливаются их скрипта, который я конечно забыл исключить из игнора)

---
+++ third_party/install_third_party.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+git clone https://github.com/ros-planning/navigation.git -b noetic-devel
+
+git clone https://github.com/tu-darmstadt-ros-pkg/hector_slam.git -b noetic-devel
+
+git clone https://github.com/ros-perception/slam_gmapping.git -b melodic-devel
+
+git clone https://github.com/5met4nka/gazebo_models_worlds_collection.git -b main
+
+git clone https://github.com/ros-controls/ros_controllers -b noetic-devel
+
+git clone https://github.com/ros-controls/ros_control -b noetic-devel
+
+git clone https://github.com/ros-drivers/four_wheel_steering_msgs.git -b master
+
+git clone https://github.com/ros-controls/urdf_geometry_parser.git -b kinetic-devel
+
+git clone https://github.com/ros/urdf.git -b melodic-devel
+
+git clone https://github.com/ros/joint_state_publisher.git -b noetic-devel
+
+git clone https://github.com/ros-simulation/gazebo_ros_pkgs.git -b noetic-devel
+
+git clone https://github.com/ros-drivers/joystick_drivers.git -b main
+
+git clone https://github.com/ros/geometry2.git -b noetic-devel
