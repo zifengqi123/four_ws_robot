0aa9106ec2dac1e9b8c4370b3f134bff70796ebe
Nikita Lobazev <pavilich99@mail.ru>
2023-06-10 13:13:07 +0300

    подредачил скрипт установки как в ros_shared

--- scripts/install_third_party.sh
+++ scripts/install_third_party.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-git -C third_party clone https://github.com/ros-planning/navigation.git -b noetic-devel
+THIRD_PARTY_DIR=third_party
+mkdir -p $THIRD_PARTY_DIR
 
-git -C third_party clone https://github.com/tu-darmstadt-ros-pkg/hector_slam.git -b noetic-devel
+git -C $THIRD_PARTY_DIR clone  https://github.com/KumarRobotics/ublox -b 1.4.1
 
-git -C third_party clone https://github.com/ros-perception/slam_gmapping.git -b melodic-devel
+git -C $THIRD_PARTY_DIR clone https://github.com/ros-planning/navigation.git -b noetic-devel
 
-git -C third_party clone https://github.com/5met4nka/gazebo_models_worlds_collection.git -b main
+git -C $THIRD_PARTY_DIR clone https://github.com/tu-darmstadt-ros-pkg/hector_slam.git -b noetic-devel
 
-git -C third_party clone https://github.com/ros-controls/ros_controllers.git -b noetic-devel
+git -C $THIRD_PARTY_DIR clone https://github.com/ros-perception/slam_gmapping.git -b melodic-devel
 
-git -C third_party clone https://github.com/ros-controls/ros_control.git -b noetic-devel
+git -C $THIRD_PARTY_DIR clone https://github.com/5met4nka/gazebo_models_worlds_collection.git -b main
 
-git -C third_party clone https://github.com/ros-drivers/four_wheel_steering_msgs.git -b master
+git -C $THIRD_PARTY_DIR clone https://github.com/ros-controls/ros_controllers.git -b noetic-devel
 
-git -C third_party clone https://github.com/ros-controls/urdf_geometry_parser.git -b kinetic-devel
+git -C $THIRD_PARTY_DIR clone https://github.com/ros-controls/ros_control.git -b noetic-devel
 
-git -C third_party clone https://github.com/ros/urdf.git -b melodic-devel
+git -C $THIRD_PARTY_DIR clone https://github.com/ros-drivers/four_wheel_steering_msgs.git -b master
 
-git -C third_party clone https://github.com/ros/joint_state_publisher.git -b noetic-devel
+git -C $THIRD_PARTY_DIR clone https://github.com/ros-controls/urdf_geometry_parser.git -b kinetic-devel
 
-git -C third_party clone https://github.com/ros-simulation/gazebo_ros_pkgs.git -b noetic-devel
+git -C $THIRD_PARTY_DIR clone https://github.com/ros/urdf.git -b melodic-devel
 
-git -C third_party clone https://github.com/ros-drivers/joystick_drivers.git -b main
+git -C $THIRD_PARTY_DIR clone https://github.com/ros/joint_state_publisher.git -b noetic-devel
 
-git -C third_party clone https://github.com/ros/geometry2.git -b noetic-devel
+git -C $THIRD_PARTY_DIR clone https://github.com/ros-simulation/gazebo_ros_pkgs.git -b noetic-devel
 
-git -C third_party clone https://github.com/ros/robot_state_publisher.git -b noetic-devel
+git -C $THIRD_PARTY_DIR clone https://github.com/ros-drivers/joystick_drivers.git -b main
+
+git -C $THIRD_PARTY_DIR clone https://github.com/ros/geometry2.git -b noetic-devel
+
+git -C $THIRD_PARTY_DIR clone https://github.com/ros/robot_state_publisher.git -b noetic-devel
