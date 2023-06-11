0c39de685034d06eb3f13c5e983c97116ff8b3fa
5met4nka <pavilich99@mail.ru>
2023-06-11 10:31:26 +0300

    добавил скачивание необходимых бинарных пакетов

---
+++ scripts/install_packages.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt install \
+    ros-$ROS_DISTRO-move-base \
+    ros-$ROS_DISTRO-amcl \
+    ros-$ROS_DISTRO-gmapping \
+    ros-$ROS_DISTRO-map-server \
+    libspnav-dev
