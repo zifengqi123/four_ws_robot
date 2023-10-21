b3e6cafa0d4e438a70dac3a86a770e10b69b587c
5met4nka <pavilich99@mail.ru>
2023-10-21 23:48:50 +0300

    перенос пакетных зависимостей в ros_shared

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,27 +2,14 @@
 
 catkin build \
     four_ws_robot \
+    four_wheel_steering_controller \
+    swerve_steering_controller \
     four_wheel_steering_msgs \
-    gazebo_ros_pkgs \
-    gazebo_ros_control \
+    urdf_geometry_parser \
     geometry2 \
     hector_slam \
     hector_imu_tools \
-    joint_state_publisher \
-    joint_state_publisher_gui \
-    joystick_drivers \
-    navigation \
-    robot_state_publisher \
-    ros_control \
-    rqt_controller_manager \
-    ros_controllers \
-    four_wheel_steering_controller \
-    slam_gmapping \
-    urdf \
-    urdf_geometry_parser \
-    robot_localization \
     hector_gazebo \
     point_cloud_converter \
-    twist_mux \
     maddrive_ros_shared \
     -j$(($(nproc)-2))
